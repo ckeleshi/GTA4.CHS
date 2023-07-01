d2d3e042dc3ce52095f34ecb4afa8f3716633889
ckeleshi <clanschen@gmail.com>
2023-07-01 17:28:34 +0800

    python version of gxt tool

---
+++ gxt_tool/json2charset.py
@@ -0,0 +1,39 @@
+﻿import struct
+import json
+import sys
+import pathlib
+import time
+
+def collect_chars(folder):
+    chars = set()
+    for j in [j for j in pathlib.Path(folder).glob('*.json') if j.is_file()]:
+        with open(pathlib.Path(folder,j),'r',encoding='utf-8') as f:
+            chars = chars.union(set(f.read()))
+
+    for ascii in range(1,256):
+        chars.discard(chr(ascii))
+    
+    chars.discard(chr(0x2122)) #trademark
+    chars.discard(chr(0x3000))
+    chars.discard(chr(0xFEFF))
+
+    return list(chars)
+
+def write_charset(chars, filename):
+    char_per_line = 64
+    char_index = 0
+    chars.sort()
+    pathlib.Path(filename).parent.mkdir(parents=True,exist_ok=True)
+    with open(filename, "w", encoding='utf-8') as f:
+        for char in chars:
+            f.write(char)
+            char_index+=1
+            if (char_index>=char_per_line):
+                f.write('\n')
+                char_index = 0
+
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+    chars = collect_chars(sys.argv[1])
+    write_charset(chars, sys.argv[2])
+    print(f'Process Time: {time.perf_counter() - start_time:.2f}s.')
---
+++ gxt_tool/json2gxt.py
@@ -0,0 +1,34 @@
+﻿import struct
+import json
+import sys
+import time
+import pathlib
+
+def load_jsons(folder):
+    data = dict()
+    for j in [j for j in pathlib.Path(folder).glob('*.json') if j.is_file()]:
+        with open(j,'r',encoding='utf-8') as f:
+            table = json.load(f)
+            for table_name in table:
+                data[table_name] = table[table_name]
+
+    return data
+
+def write_gxt(tables,filename):
+    pathlib.Path(filename).parent.mkdir(parents=True,exist_ok=True)
+    with open(filename,"wb") as f:
+        f.write(b'\x04\x00\x10\x00')
+        f.write(b'TABL')
+        f.write(struct.pack('I', len(tables)*12))
+        #预留table data的空隙，先写入TKEY & TDAT并计算出Table的Offset值，再一次性写入TablBlock
+        tkey_block_offset = 12+len(tables)*12
+        f.seek(tkey_block_offset)
+        f.write(b'TKEY')
+        for table in tables.items():
+            
+
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+    tables = load_jsons(sys.argv[1])
+    write_gxt(tables, sys.argv[2])
+    print(f'Process Time: {time.perf_counter() - start_time:.2f}s.')
